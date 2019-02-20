387a9e7812048705d985b4962f3ea16092b51ea4
NyaKahjiit <47828725+NyaKahjiit@users.noreply.github.com>
2019-02-21 01:26:41 +0300

    popil

---
+++ Assets/Prefabs/Resources/GameZone/InitZone.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitZone : MonoBehaviour
+{
+    public void Start()
+    {
+        GameStart size = new GameStart();
+        LObject.LZone go = new LObject.LZone();
+        GameObject Field = go.LField();
+        GameObject WallBlock = go.LWall();
+        float zx = 0, zy = 0;
+        for (int i = 0; i < size.x_count ; i++)
+        {
+            zx += 0.2f;
+            zy = 0;
+            for (int j = 0; j < size.y_count; j++)
+            {
+                if (i == 0 | j == 0 | i == size.x_count - 1 | j == size.y_count - 1 | (i%2==0 & j%2==0))
+                {
+                    Instantiate(WallBlock, new Vector3(i+zx, 0.5f, j+zy), Quaternion.identity);
+                }
+                zy += 0.2f;
+            }
+        }
+        Instantiate(Field, new Vector3(0, 0, 0), Quaternion.identity);
+        Debug.Log(size.x_count);
+    }
+
+}
