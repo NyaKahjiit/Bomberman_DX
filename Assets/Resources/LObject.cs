e4709963816a9e0fd67e01352064bd26ccb8e793
NyaKahjiit <47828725+NyaKahjiit@users.noreply.github.com>
2019-02-20 21:32:16 +0300

    Initial commit

---
+++ Assets/Resources/LObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LObject : MonoBehaviour
+{
+    public GameObject LZone()
+    {
+        GameObject zone = Resources.Load("GameZone") as GameObject;
+        return zone;
+    }
+    public GameObject LWall()
+    {
+        GameObject wall = Resources.Load("Cube") as GameObject;
+        return wall;
+    }
+
+}
