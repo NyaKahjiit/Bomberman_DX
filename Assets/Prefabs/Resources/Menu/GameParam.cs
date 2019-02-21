a3b79fe32803d6b1482303c47ac517b9e98dbbf6
NyaKahjiit <47828725+NyaKahjiit@users.noreply.github.com>
2019-02-21 23:14:09 +0300

    Start process making game menu

---
+++ Assets/Prefabs/Resources/Menu/GameParam.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameParam : MonoBehaviour
+{
+    public string Text;
+    public int X_count=9, Y_count=9;
+    public void INsert_X()
+    {
+        GameObject.Find("scaleField_x");
+        string x= this.Text;
+        Debug.Log(x);
+    }
+}
---
+++ Assets/Prefabs/Resources/Menu/menucontrols.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menucontrols : MonoBehaviour
+{
+    public void PresstoPlay()
+    {
+        SceneManager.LoadScene("Game");
+    }
+    public void ExitPressed()
+    {
+        Application.Quit();
+        Debug.Log("Exit pressed!");
+    }
+}
