633c942201b410cbbce22be60ea5b65b033fed39
NyaKahjiit <47828725+NyaKahjiit@users.noreply.github.com>
2019-02-27 04:03:45 +0300

    created start zone to player
    Fixed many Shit

--- Assets/Prefabs/Resources/GameLoc/GameZone/InitZone.cs
+++ Assets/Prefabs/Resources/GameLoc/GameZone/InitZone.cs
@@ -4,63 +4,81 @@ using UnityEngine;
 
 public class InitZone : MonoBehaviour
 {
+    List<Vector3> EmptyXYMass = new List<Vector3>();
     public float SpaceBlock;
     public GameObject Playerobj;
     public GameObject Field;
     public GameObject WallBlock;
     public GameObject Box;
     public static GameObject Bomb;
+    public GameObject Err;
+    public int chancecreatebox;
     public void Start()
     {
-        Random rand = new Random();
-        List<Vector3> EmptyXYMass = new List<Vector3>();
-        List<Vector3> playerneiqhbour = new List<Vector3>();
+        Generated_static_object();
+        Generated_dynamic_obj();
+    }
+    void Generated_static_object()
+    {
         Vector3 EmptyXY = new Vector3();
         int X_count = PlayerPrefs.GetInt("X_count");
         int Y_count = PlayerPrefs.GetInt("Y_count");
-        Debug.Log("Your setting for field(Cubes): x="+ X_count+", y="+Y_count);
-        float zx = 0, zy = 0, x_field=0, y_field=0;
-        for (int i = 0; i < X_count ; i++)
+        Debug.Log("Your setting for field(Cubes): x=" + X_count + ", y=" + Y_count);
+        float spaceblockx = 0, spaceblocky = 0, x_field = 0, y_field = 0;
+        for (int i = 0; i < X_count; i++)
         {
-            zy = 0;
+            spaceblocky = 0;
             for (int j = 0; j < Y_count; j++)
             {
-                if (i == 0 | j == 0 | i == X_count - 1 | j == Y_count - 1 | (i%2==0 & j%2==0))
+                if (i == 0 | j == 0 | i == X_count - 1 | j == Y_count - 1 | (i % 2 == 0 & j % 2 == 0))
                 {
-                    Instantiate(WallBlock, new Vector3(i+zx, 0.5f, j+zy), Quaternion.identity);
+                    Instantiate(WallBlock, new Vector3(i + spaceblockx, 0.5f, j + spaceblocky), Quaternion.identity);
                 }
                 else
                 {
-                    EmptyXY = new Vector3(i + zx, 0.5f, j + zy);
+                    EmptyXY = new Vector3(i + spaceblockx, 0.5f, j + spaceblocky);
                     EmptyXYMass.Add(EmptyXY);
                 }
                 if (i == X_count - 1 & j == Y_count - 1)
                 {
-                    x_field = (i + zx);
-                    y_field = (j + zy);
+                    x_field = (i + spaceblockx);
+                    y_field = (j + spaceblocky);
                 }
-                zy += SpaceBlock;
+                spaceblocky += SpaceBlock;
             }
-            zx += SpaceBlock;
+            spaceblockx += SpaceBlock;
         }
+        GameObject newObject = Instantiate(Field, new Vector3((x_field) / 2, 0, (y_field) / 2), Quaternion.identity) as GameObject;
+        newObject.transform.localScale = new Vector3((x_field + 1) / 10, 1, (y_field + 1) / 10);
+    }
+    void Generated_dynamic_obj()
+    {
+        List<Vector3> playerneiqhbour = new List<Vector3>();
         int pos = Random.Range(1, EmptyXYMass.Count);
         Vector3 playerpos = EmptyXYMass[pos];
-        playerneiqhbour.Add(playerpos+new Vector3(1.1f, 0, 0));
-        GameObject newObject = Instantiate(Field, new Vector3((x_field)/2 , 0, (y_field)/2), Quaternion.identity) as GameObject;
-        newObject.transform.localScale = new Vector3((x_field+1)/10 , 1, (y_field+1) / 10);
-        Instantiate(Playerobj , EmptyXYMass[pos], Quaternion.identity);
+        Instantiate(Playerobj, EmptyXYMass[pos], Quaternion.identity);
         EmptyXYMass.RemoveAt(pos);
-
-        for (int i =0; i<=EmptyXYMass.Count-1; i++)
+        Vector3 XY_neiqhbour = playerpos;
+        for(int i=0; i<=2; i++)
         {
-            if(Random.Range(0,100)<=50)
+            foreach (Vector3 deb in EmptyXYMass)
             {
-                if (playerneiqhbour[0]!= EmptyXYMass[i])
+                if ((deb == XY_neiqhbour + new Vector3(1 + SpaceBlock, 0, 0)) | (deb == XY_neiqhbour - new Vector3(1 + SpaceBlock, 0, 0)) | (deb == XY_neiqhbour + new Vector3(0, 0, 1 + SpaceBlock)) | (deb == XY_neiqhbour - new Vector3(0, 0, 1 + SpaceBlock)))
                 {
-                    Instantiate(Box, EmptyXYMass[i], Quaternion.identity);
+                    playerneiqhbour.Add(deb);
                 }
             }
+            XY_neiqhbour = playerneiqhbour[Random.Range(0, playerneiqhbour.Count)];
+            EmptyXYMass.Remove(XY_neiqhbour);
+            playerneiqhbour.Clear();
         }
-        Debug.Log("player created on = "+playerpos);
+        for (int i = 0; i <= EmptyXYMass.Count - 1; i++)
+        {
+            if (Random.Range(0, 100) <= chancecreatebox)
+            {
+                Instantiate(Box, EmptyXYMass[i], Quaternion.identity);
+            }
+        }
+        Debug.Log("player created on = " + playerpos);
     }
 }
